4cbd83bf5dda931a092fd989d02f16eb3992ab28
Jheckryz Maison Osio <osiojheckryz@gmail.com>
2025-04-18 04:36:56 +0800

    Dark mode fix. image placement, and profile placeholder

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
 let darkmode = localStorage.getItem('darkmode')
 const themeSwitch = document.getElementById('theme-switch')
+const modeImage = document.getElementById('mode-image')
+
+
+const setImageMode = () => {
+  modeImage.src = document.body.classList.contains('darkmode')
+    ? 'images/dark-image.png'
+    : 'images/light-image.png'
+}
 
 const enableDarkmode = () => {
-    document.body.classList.add('darkmode')
-    localStorage.setItem('darkmode','active')
+  document.body.classList.add('darkmode')
+  localStorage.setItem('darkmode','active')
+  setImageMode()
 }
 
 const disableDarkmode = () => {
-    document.body.classList.remove('darkmode')
-    localStorage.setItem('darkmode',null)
+  document.body.classList.remove('darkmode')
+  localStorage.setItem('darkmode', null)
+  setImageMode()
 }
 
+
 if (darkmode === "active") enableDarkmode()
+else setImageMode()
+
 
 themeSwitch.addEventListener("click", () => {
-    darkmode = localStorage.getItem('darkmode')
-    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+  darkmode = localStorage.getItem('darkmode')
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode()
 })
